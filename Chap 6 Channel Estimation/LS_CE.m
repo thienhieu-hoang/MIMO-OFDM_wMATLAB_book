c29c799b66bfde71939fe44e80aee2f41701a73c
Hieu Hoang <79343969+hieuhoanghust@users.noreply.github.com>
2024-06-19 15:31:30 -0400

    change

--- Chap 6 Channel Estimation/LS_CE.m
+++ Chap 6 Channel Estimation/LS_CE.m
@@ -11,6 +11,6 @@ function [H_LS] = LS_CE(Y,Xp,pilot_loc,Nfft,Nps,int_opt)
 % H_LS = LS Channel estimate
 Np=Nfft/Nps; k=1:Np;
 LS_est(k) = Y(pilot_loc(k))./Xp(k); % LS channel estimation
-if lower(int_opt(1))==’l’, method=’linear’; else method=’spline’; end
+if lower(int_opt(1))=='l', method='linear'; else method='spline'; end
 % Linear/Spline interpolation
 H_LS = interpolate(LS_est,pilot_loc,Nfft,method);
